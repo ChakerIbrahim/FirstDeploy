bdeea4d315c718963c86ef6ff8c084cd447d98be
ChakerIbrahim <chaker703@hotmail.com>
2026-06-07 23:12:14 +0300

    first

--- ninja_gold/settings.py
+++ ninja_gold/settings.py
@@ -4,9 +4,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 SECRET_KEY = 'django-insecure-ninja-gold-change-this-in-production'
 
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = ['localhost', '127.0.0.1']
+ALLOWED_HOSTS = ['16.171.43.122']
 
 INSTALLED_APPS = [
     'django.contrib.admin',
